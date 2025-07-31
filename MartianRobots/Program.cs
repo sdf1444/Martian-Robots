84425ebb1ec1c38028cc2e39102b3073016497ef
Spencer Du <spencer@wearelighten.co.uk>
2025-08-01 00:36:29 +0100

    Add Program entry point to start the application

---
+++ MartianRobots/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MartianRobots
+{
+    /// <summary>
+    /// Application entry point.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            var controller = new RobotController();
+            controller.ProcessInput();
+        }
+    }
+}
