a19ba2d486367f1ff8a50e37bb4c35a8026aa9e7
Spencer Du <spencer@wearelighten.co.uk>
2025-08-01 00:05:52 +0100

    Initial commit: Add Models folder and Direction enum

---
+++ MartianRobots/Models/Direction.cs
@@ -0,0 +1,13 @@
+﻿namespace MartianRobots.Models
+{
+    /// <summary>
+    /// Enum representing the four possible orientations of a robot.
+    /// </summary>
+    public enum Direction
+    {
+        N, // Facing North
+        E, // Facing East
+        S, // Facing South
+        W  // Facing West
+    }
+}
